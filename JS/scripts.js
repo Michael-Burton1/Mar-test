baea4a7bb217cda3788eceaf1fa1adadf4c9379d
Michael Burton <micbur1@gmail.com>
2021-03-22 13:01:57 -0700

    create project with html css and scripts files
    co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    co-authored-by: Rayn Walker <ryandanielwalker@gmail.com>

---
+++ JS/scripts.js
@@ -0,0 +1,31 @@
+// alert("Jamie!!!!!!");
+// prompt("How are you?");
+// const answer = prompt("what is your name");
+// alert(answer+", YOU JUST WON $1000");
+
+// function saySomething(whatToSay) {
+//   alert(whatToSay);
+// }
+
+// saySomething("steve");
+
+
+// let inputName = prompt("what is your name?");
+
+// function greeting() {
+//   alert("Hola, "+ inputName);
+// };
+
+// greeting();
+
+const num1= 6;
+const num2= 4;
+
+function mulitply() {
+  return num1 * num2;
+};
+
+mulitply();
+
+
+
