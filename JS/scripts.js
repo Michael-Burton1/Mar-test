25172e428ad07f7085eef54f476742215e20badc
Michael Burton <micbur1@gmail.com>
2021-03-23 11:14:53 -0700

    add index2 and change scripts and styles for 'collor lesson co-authored-by: Min Chang <minchangmhc@gmail.com> co-authored-by: Eric Roragen-Eggers <Eric.D.Eggers@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,22 @@
-$(document).ready(function(){
-  $(".clickable").click(function(){
-    $("#walrus-showing").slideToggle();
-    $("#walrus-hidden").slideToggle();
+// $(document).ready(function(){
+//   $(".clickable").click(function(){
+//     $("#walrus-showing").slideToggle();
+//     $("#walrus-hidden").slideToggle();
+//   });
+// });
+
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").addClass("red-background");
   });
 });
-
 
