7aaa521ddf8e9c919c88d5ac29b1e71dfefa634b
Michael Burton <micbur1@gmail.com>
2021-03-23 11:56:48 -0700

    add dark mode button and funtion to project co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com> co-authored-by: Rayn Walker <ryandanielwalker@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -8,15 +8,23 @@
 
 $(document).ready(function() {
   $("button#green").click(function() {
+    $("body").removeClass();
     $("body").addClass("green-background");
   });
 
   $("button#yellow").click(function() {
+    $("body").removeClass();
     $("body").addClass("yellow-background");
   });
 
   $("button#red").click(function() {
+    $("body").removeClass();
     $("body").addClass("red-background");
+  });
+
+  $("button#dMode").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-mode");
   });
 });
 
