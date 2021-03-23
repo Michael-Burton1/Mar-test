4f6cdf3c7f44cd2572f409425333316fdaca6df4
Michael Burton <micbur1@gmail.com>
2021-03-23 10:57:31 -0700

    change toggle function to slide toggle in scripts co-authored-by: Min Chang <minchangmhc@gmail.com> co-authored-by: Eric Roragen-Eggers <Eric.D.Eggers@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $(".clickable").click(function(){
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
   });
 });
 
