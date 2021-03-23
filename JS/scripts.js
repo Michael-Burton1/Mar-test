bbc23ff2150da0140386e34c3211975b94f91a34
Michael Burton <micbur1@gmail.com>
2021-03-23 10:19:27 -0700

    start peek-a-boo project
    co-authored-by: Min Chang <minchangmhc@gmail.com>
    co-authored-by: Eric Roragen-Eggers <Eric.D.Eggers@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,32 +1,8 @@
-// alert("Jamie!!!!!!");
-// prompt("How are you?");
-// const answer = prompt("what is your name");
-// alert(answer+", YOU JUST WON $1000");
-
-// function saySomething(whatToSay) {
-//   alert(whatToSay);
-// }
-
-// saySomething("steve");
-
-
-// let inputName = prompt("what is your name?");
-
-// function greeting() {
-//   alert("Hola, "+ inputName);
-// };
-
-// greeting();
-
-let n1= parseInt(prompt("type a number"));
-let n2= 4;
-
-function mulitply() {
-  alert(n1 * n2);
-  return n1 * n2;
-};
-
-mulitply();
-
+$(document).ready(function(){
+  $("p").click(function(){
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+});
 
 
